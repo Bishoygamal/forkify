cf77256b93778acfddbd200a1368494140db63d0
Bisho Gamal <beshoy.ot@gmail.com>
2020-01-14 18:47:44 +0200

    second

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
-// Global app controller
-import num from './test';
-
-console.log(`i imported ${num} from another module`);
+import axios from 'axios';
+async function getResults(query){
+    try{
+        const Res =await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const Results = Res.data.recipes;
+        console.log(Results);
+    }
+    catch(error){
+        alert(error)
+    }
+} 
+getResults('pizza');
